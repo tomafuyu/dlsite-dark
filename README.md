a1be7ab9cf79837ac8133f14d790208823ce4772
tomafuyu <mafuyuki@mfy-win11-nkr>
2024-02-29 04:30:01 +0900

    first

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dlsite-dark
+
+Maybe, it is the most simple script for dark mode on dlsite.
+Use any extensions to install, that can execute scripts.
+`UserScript` is for (Tampermonkey)[https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo].
+Change the value `b` and `c` to customize color.
---
+++ script.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         dlsite darkmode
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-28
+// @description  try to take over the world!
+// @author       toma*fuyu
+// @match        https://www.dlsite.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dlsite.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    "use strict";
+    
+    const $$ = (q, a=false) => a ?
+        document.querySelectorAll(q)
+        : document.querySelector(q);
+    
+    /* ===== User Settings ===== */
+    const b = "#111";
+    const c = "aliceblue";
+    /* ===== / User Settings ===== */
+    
+    $$("body").style.color = c;
+    $$("body").style.backgroundColor = b;
+
+    // header
+    $$("input#search_text").style.color = c;
+    $$("input#search_text").style.backgroundColor = b;
+    $$("div.headerCore-mainInner").style.backgroundColor = b;
+    $$("div.floorSubNav").style.backgroundColor = b;
+
+    // left
+    $$("ul.list_content_text.type-icon").style.backgroundColor = b;
+
+    $$("#container").style.color = c;
+    $$("#wrapper").style.color = c;
+    
+    $$("head").innerHTML += `
+        <style>
+            #left li a {
+                color: aliceblue;
+                &:hover {
+                    background-color: #444;
+                }
+            }
+            a.work_name,
+            .work_name a {
+                color: lightskyblue;
+            }
+        </style>
+    `;
+})();
